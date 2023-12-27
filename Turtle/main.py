2910f7ed0d7f3eae835d9aff7dbbbe1c59ba5ffe
Pratik Shrestha <pratik.shrestha@cotiviti.com>
2023-12-27 15:06:44 +0545

    Add function to draw multiple shapes

--- Turtle/main.py
+++ Turtle/main.py
@@ -7,18 +7,34 @@ screen = Screen()
 
 # timmy.color("red")
 
-# Drawing a square
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+# # Drawing a square
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
 
-screen.clear()
+# screen.clear()
 
-# Drawing a dashed line
-for _ in range(10):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
+# # Drawing a dashed line
+# for _ in range(10):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
+
+# screen.clear()
+
+# Drawing shapes
+
+
+def draw_shape(sides):
+    RADIUS = 360
+
+    for _ in range(sides):
+        timmy.forward(100)
+        timmy.right(RADIUS/sides)
+
+
+for no_of_shape in range(3, 9):
+    draw_shape(no_of_shape)
 
 screen.exitonclick()
