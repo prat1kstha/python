74879d9709f080118fef7b59af9c5ef1b0d8ff73
Pratik Shrestha <animezon.stha@gmail.com>
2024-01-05 21:32:46 +0545

    pandas initial code

--- Panda/WeatherData/main.py
+++ Panda/WeatherData/main.py
@@ -13,5 +13,37 @@
 
 import pandas
 
+# read data from csv
 data = pandas.read_csv("Panda/WeatherData/002 weather-data.csv")
-print(data["temp"])
+# print(data["temp"])
+
+# convert data to dictionary
+data_dict = data.to_dict()
+# print(data_dict)
+
+# covert column to list
+temp_list = data["temp"].to_list()
+
+# aggregate functions
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# get data in a row
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+print((monday_temp * 9/5) + 32)
+
+
+# create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data_frame = pandas.DataFrame(data_dict)
+print(data_frame)
+
+data_frame.to_csv("Panda/WeatherData/new_data.csv")
