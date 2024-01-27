bd218e18224c5c6bb39d7e0158dcfacffd73ddc8
Pratik Shrestha <animezon.stha@gmail.com>
2024-01-27 17:15:22 +0545

    grid implementation

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -3,14 +3,16 @@ import tkinter
 window = tkinter.Tk()
 
 window.title("My Program")
-window.minsize(width=500, height=500)
+window.minsize(width=500, height=300)
+
+window.config(padx=20, pady=20)
 
 # Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
-my_label.pack()
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+# my_label.pack()
 
-my_label["text"] = "New Text"
 my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
 
 
 # Button
@@ -18,18 +20,16 @@ def button_clicked():
     new_text = input.get()
     my_label.config(text=new_text)
 
-button = tkinter.Button(text="Click Me", command=button_clicked)
-button.pack()
+button1 = tkinter.Button(text="Click Me", command=button_clicked)
+button1.grid(column=1, row=1)
+
+button2 = tkinter.Button(text="Click Me 2", command=button_clicked)
+button2.grid(column=2, row=0)
 
 
 # Entry
 input = tkinter.Entry(width=10)
-
-input.pack()
-
-# import turtle
-
-# tim = turtle.Turtle()
-# # tim.write()
+input.grid(column=3, row=2)
+# input.pack()
 
 window.mainloop()
