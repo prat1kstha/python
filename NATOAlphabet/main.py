3788ffc7aabd075fc70f0ef34ac2271fbbddbb08
Pratik Shrestha <animezon.stha@gmail.com>
2024-03-22 17:53:11 +0545

    Error handling added in NATO Alphabet

--- NATOAlphabet/main.py
+++ NATOAlphabet/main.py
@@ -26,7 +26,16 @@ data = pandas.read_csv("NATOAlphabet/nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-input_word = input("Write a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in input_word]
 
-print(output_list)
+def generate_phonetic():
+    input_word = input("Write a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in input_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()        
+    else:
+        print(output_list)
+
+generate_phonetic()
