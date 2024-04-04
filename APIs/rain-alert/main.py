8c7e0d3f7ac2775ab2060799bc0ff48e8bb9ad00
Pratik Shrestha <animezon.stha@gmail.com>
2024-04-04 22:19:07 +0545

    added rain-alert program

---
+++ APIs/rain-alert/main.py
@@ -0,0 +1,37 @@
+import requests
+
+OWN_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key="d14a82f4bc69daa539146d54a663d82c"
+
+weather_params = {
+    "lat": "27.66641253286814",
+    "lon": "85.33086143678096",
+    "appid": api_key
+}
+
+response = requests.get(OWN_Endpoint, params=weather_params)
+response.raise_for_status()
+
+# print(response.status_code)
+
+weather_data = response.json()
+
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+weather_slice = weather_data["list"][:8]
+# print(weather_slice)
+
+will_rain = False
+time = ""
+
+for qtr in weather_slice:
+    # time = qtr["dt_txt"]
+    condition_code = qtr["weather"][0]["id"]
+
+    if int(condition_code) < 700:
+        time = qtr["dt_txt"]
+        will_rain = True
+        break
+
+if will_rain:
+    print(f"Bring an umbrella. It might rain at {time}")
